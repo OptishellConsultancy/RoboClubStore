1e8d5b25451bcf9c3602b2adf0afb7e4e04d05d9
JamieShelley <sirshelley@hotmail.com>
2022-03-10 04:16:01 +0000

    Text to speach startup for GPIOToggleOnoFF.PY

--- GPIOToggleOnOff.py
+++ GPIOToggleOnOff.py
@@ -1,7 +1,11 @@
+#!/usr/bin/python
 # coding: utf-8
 
-# sudo nano /etc/rc.local
-# sudo python /home/pi/Desktop/RoboClubStore/GPIOToggleOnOff.py &
+# To run on start:
+# sudo raspi-config -> system options -> Boot / Auto Login ->  Console Autologin”
+# sudo nano /etc/profile
+# add: sudo python /home/pi/Desktop/RoboClubStore/GPIOToggleOnOff.py &
+# CTRL+X
 
 from gpiozero import Button #import button from the Pi GPIO library
 import time # import time functions
@@ -9,10 +13,44 @@ import os #imports OS library for Shutdown control
 
 
 
-speak = "Hello world"
-os.popen('espeak "' + speak + '" --stdout | aplay 2> /dev/null').read()
+def check_privileges():
+    if not os.environ.get("SUDO_UID") and os.geteuid() != 0:
+        raise PermissionError("You need to run this script with sudo or as root.")
 
-print('Done speech')
+check_privileges()
+
+# http://espeak.sourceforge.net/commands.html
+def shellESpeak(text):
+
+    os.popen('espeak "' + text + '" --stdout | aplay 5> /dev/null').read()
+
+shellESpeak("Welcome to the mutlipurpose platform.")
+shellESpeak("I currently have the following functionality: ")
+shellESpeak("GPS querying")
+shellESpeak("Accelerometer, Magnetometer")
+shellESpeak("3 meter sonic range finder")
+shellESpeak("HD Camera with 2 axis pan tilt and webserver streaming option")
+shellESpeak("Four wheeled drive")
+shellESpeak("Six Axis High torque arm")
+shellESpeak("OLED Screen")
+shellESpeak("Microphone")
+shellESpeak("Speaker")
+shellESpeak("Spotify integration")
+
+time.sleep(0.5)
+shellESpeak("Please consult 'APIExamples.txt' for API documentation on these features")
+
+time.sleep(1)
+shellESpeak("GPIO Runtime initialised.")
+time.sleep(0.5)
+shellESpeak("Press button for shutdown.")
+shellESpeak("Double press Button for restart.")
+time.sleep(0.5)
+shellESpeak("Please consult 'APIExamples.txt' for API documentation")
+
+shellESpeak("I am now completely operational Dave, and all my circuits are functionin perfectly")
+time.sleep(0.2)
+shellESpeak("Just Kidding")
 
 btn = Button(26) # defines the button as an object and chooses GPIO 26
 shutdownButtonHoldTime = 5
@@ -22,6 +60,7 @@ while True: #infinite loop
         time.sleep(1) # wait for the hold time we want. 
         if btn.is_pressed: #check if the user let go of the button
             print("pressed and held, shutting down")
+            shellESpeak("shuting down.")
             os.system("shutdown now -h") #shut down the Pi -h is or -r will reset
         time.sleep(1) # wait to loop again so we don’t use the processor too much.
     else:
@@ -29,4 +68,5 @@ while True: #infinite loop
         btn.wait_for_release()
         if btn.wait_for_press(timeout=0.6):
             print("pressed twice, restarting")
+            shellESpeak("Restarting.")
             os.system("shutdown -r now")
